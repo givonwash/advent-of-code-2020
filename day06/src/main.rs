d843fe6f777d0823629d5a20c6906ea366b5bb98
givonwash <givonwash@gmail.com>
2021-11-26 21:25:30 -0600

    refactor day 6 solution

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -23,19 +23,24 @@ where
 {
     let answer: usize = groups
         .map(|group| {
-            let mut char_frequency = HashMap::new();
-            let mut people = 1;
-            for c in group.trim_end().chars() {
-                if c == '\n' {
-                    people += 1;
-                } else {
-                    let count = char_frequency.entry(c).or_insert(0);
-                    *count += 1;
-                }
-            }
-            char_frequency
-                .into_values()
-                .filter(|val| *val == people)
+            let (people, question_frequency) = group.lines().fold(
+                (0, HashMap::new()),
+                |(people, mut question_frequency), questions| {
+                    for question in questions.chars() {
+                        question_frequency
+                            .entry(question)
+                            .and_modify(|freq| {
+                                *freq += 1;
+                            })
+                            .or_insert(1);
+                    }
+                    (people + 1, question_frequency)
+                },
+            );
+
+            question_frequency
+                .values()
+                .filter(|freq| **freq == people)
                 .count()
         })
         .sum();
@@ -45,7 +50,9 @@ where
 fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+
     let groups = input.split("\n\n");
+
     part_one(groups.clone());
     part_two(groups);
     Ok(())
