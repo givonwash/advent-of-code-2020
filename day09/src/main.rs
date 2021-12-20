62ec3670c89e2e5022100cce727ce10c4b0773b2
givonwash <givonwash@gmail.com>
2021-12-19 16:23:31 -0800

    Add day 9 solution

---
+++ day09/src/main.rs
@@ -0,0 +1,100 @@
+use std::{
+    cmp::Ordering,
+    collections::{HashSet, VecDeque},
+    io::{self, Read},
+};
+
+fn find_xy(nums: &[u64], target: u64) -> Option<(u64, u64)> {
+    let mut complements = HashSet::new();
+
+    nums.iter().filter(|n| **n < target).find_map(|n| {
+        if let Some(c) = complements.get(&(target - n)) {
+            if n != c {
+                Some((*n, *c))
+            } else {
+                None
+            }
+        } else {
+            complements.insert(*n);
+            None
+        }
+    })
+}
+
+fn part_one(nums: &[u64]) {
+    let first_fail = nums
+        .windows(26)
+        .find_map(|win| {
+            let (win, num) = win.split_at(25);
+            let num = num.first().unwrap();
+            match find_xy(win, *num) {
+                Some(..) => None,
+                None => Some(num),
+            }
+        })
+        .expect("No invalid numbers in input");
+
+    println!("Part One: {}", first_fail);
+}
+
+fn part_two(nums: &[u64]) {
+    let (first_fail_at, first_fail) = nums
+        .windows(26)
+        .enumerate()
+        .find_map(|(i, win)| {
+            let (win, num) = win.split_at(25);
+            let num = num.first().unwrap();
+            match find_xy(win, *num) {
+                Some(..) => None,
+                None => Some((i, num)),
+            }
+        })
+        .expect("No invalid numbers in input");
+
+    let mut contig = VecDeque::new();
+    let mut cand = nums.iter().take(first_fail_at - 1);
+
+    let ans = loop {
+        let (min, max, sum): (Option<u64>, Option<u64>, u64) =
+            contig
+                .iter()
+                .fold((None, None, 0), |(mut amin, mut amax, mut asum), n| {
+                    amin = amin.map(|mn| mn.min(*n)).or(Some(*n));
+                    amax = amax.map(|mn| mn.max(*n)).or(Some(*n));
+                    asum += *n;
+                    (amin, amax, asum)
+                });
+        match sum.cmp(first_fail) {
+            Ordering::Greater => {
+                contig.pop_front();
+            }
+            Ordering::Equal => {
+                break Some(min.unwrap() + max.unwrap());
+            }
+            Ordering::Less => {
+                if let Some(num) = cand.next() {
+                    contig.push_back(*num);
+                } else {
+                    break None;
+                }
+            }
+        }
+    };
+
+    println!("Part Two: {:?}", ans);
+}
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let nums = input
+        .lines()
+        .map(|num| num.parse().unwrap())
+        .collect::<Vec<_>>();
+
+    part_one(&nums);
+    part_two(&nums);
+
+    Ok(())
+}
