2000824991ca7ac665019793b66d889ecab2d877
givonwash <givonwash@gmail.com>
2022-12-24 22:26:52 -0800

    refactor(solutions): simplify day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,40 +1,45 @@
 use std::collections::HashSet;
 use std::io::{self, Read};
 
-fn find_xy(nums: &[i32], target: i32) -> Option<(i32, i32)> {
+fn find_xy(mut nums: impl Iterator<Item = i32>, target: i32) -> Option<(i32, i32)> {
     let mut complements = HashSet::new();
 
-    nums.iter().find_map(|n| {
+    nums.find_map(|n| {
         if let Some(c) = complements.get(&(target - n)) {
-            Some((*n, *c))
+            Some((n, *c))
         } else {
-            complements.insert(*n);
+            complements.insert(n);
             None
         }
     })
 }
 
 fn part_one(expenses: &[i32]) {
-    println!(
-        "Part One: {:?}",
-        find_xy(expenses, 2020).map(|(x, y)| x * y)
-    );
+    let answer = find_xy(expenses.iter().copied(), 2020).map(|(x, y)| x * y);
+
+    println!("Part One: {answer:?}");
 }
 
 fn part_two(expenses: &[i32]) {
-    let mut attempted = HashSet::new();
-    let mut exp_iter = expenses.iter();
+    let mut seen = HashSet::new();
 
-    let ans = loop {
-        if let Some(z) = exp_iter.next().filter(|z| attempted.insert(*z)) {
-            if let Some((x, y)) = find_xy(expenses, 2020 - z) {
-                break Some(x * y * z);
-            }
-        } else {
-            break None;
-        }
-    };
-    println!("Part Two: {:?}", ans);
+    let answer = expenses
+        .iter()
+        .enumerate()
+        .filter(|(_, e)| seen.insert(**e))
+        .find_map(|(i, e)| {
+            find_xy(
+                expenses
+                    .iter()
+                    .copied()
+                    .enumerate()
+                    .filter_map(|(j, e)| (i != j).then(|| e)),
+                2020 - e,
+            )
+            .map(|(x, y)| x * y * e)
+        });
+
+    println!("Part Two: {answer:?}");
 }
 
 fn main() -> io::Result<()> {
