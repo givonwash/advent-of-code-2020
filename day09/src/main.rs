ab2e97a4be200675db40c9f3f64a71d70f2791ce
givonwash <givonwash@gmail.com>
2022-12-27 12:33:17 -0800

    refactor(solutions): simplify day 9

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -21,6 +21,30 @@ fn find_xy(nums: &[u64], target: u64) -> Option<(u64, u64)> {
     })
 }
 
+fn find_weakness(nums: &[u64], start: usize, end: usize, target: u64) -> Option<u64> {
+    let (min, max, sum) = &nums[start..end].iter().fold(
+        (None, None, 0),
+        |(mut amin, mut amax, mut asum): (Option<u64>, Option<u64>, u64), n| {
+            amin = amin.map(|mn| mn.min(*n)).or(Some(*n));
+            amax = amax.map(|mx| mx.max(*n)).or(Some(*n));
+            asum += *n;
+            (amin, amax, asum)
+        },
+    );
+
+    match sum.cmp(&target) {
+        Ordering::Greater => find_weakness(nums, start + 1, end, target),
+        Ordering::Equal => Some(min.unwrap() + max.unwrap()),
+        Ordering::Less => {
+            if end < nums.len() - 1 {
+                find_weakness(nums, start, end + 1, target)
+            } else {
+                None
+            }
+        }
+    }
+}
+
 fn part_one(nums: &[u64]) {
     let first_fail = nums
         .windows(26)
@@ -34,7 +58,7 @@ fn part_one(nums: &[u64]) {
         })
         .expect("No invalid numbers in input");
 
-    println!("Part One: {}", first_fail);
+    println!("Part One: {first_fail}");
 }
 
 fn part_two(nums: &[u64]) {
@@ -51,37 +75,9 @@ fn part_two(nums: &[u64]) {
         })
         .expect("No invalid numbers in input");
 
-    let (mut start, mut end) = (0, 1);
-    let mut cand = nums.iter().take(first_fail_at - 1);
+    let answer = find_weakness(&nums[..first_fail_at], 0, 1, *first_fail);
 
-    let ans = loop {
-        let (min, max, sum) = &nums[start..end].iter().fold(
-            (None, None, 0),
-            |(mut amin, mut amax, mut asum): (Option<u64>, Option<u64>, u64), n| {
-                amin = amin.map(|mn| mn.min(*n)).or(Some(*n));
-                amax = amax.map(|mn| mn.max(*n)).or(Some(*n));
-                asum += *n;
-                (amin, amax, asum)
-            },
-        );
-        match sum.cmp(first_fail) {
-            Ordering::Greater => {
-                start += 1;
-            }
-            Ordering::Equal => {
-                break Some(min.unwrap() + max.unwrap());
-            }
-            Ordering::Less => {
-                if cand.next().is_some() {
-                    end += 1;
-                } else {
-                    break None;
-                }
-            }
-        }
-    };
-
-    println!("Part Two: {:?}", ans);
+    println!("Part Two: {answer:?}");
 }
 
 fn main() -> io::Result<()> {
