af1770219a3b74f9d52bf2b0271ff264a0131877
givonwash <givonwash@gmail.com>
2022-12-29 14:19:36 -0800

    refactor(solutions): simplify day 10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,11 +3,74 @@ use std::{
     io::{self, Read},
 };
 
+fn count_arrangements(adapters: &[u32]) -> Option<u64> {
+    fn counter(adapters: &[u32], cache: &mut HashMap<u32, Option<u64>>) -> Option<u64> {
+        adapters.last().and_then(|head| {
+            cache.get(head).copied().unwrap_or_else(|| {
+                let arrangements = match adapters {
+                    [] | [_] => Some(1),
+                    &[prev, _] => head.checked_sub(prev).filter(|d| *d <= 3).and(Some(1)),
+                    &[prev2, prev1, _] => {
+                        match (head.checked_sub(prev2), head.checked_sub(prev1)) {
+                            // `prev2` and `prev1` are both reachable
+                            (Some(2..=3), Some(1..=2)) => {
+                                counter(&adapters[..2], cache).map(|c| c + 1)
+                            }
+                            // `prev2` is unreachable & `prev1` is reachable
+                            (Some(4..), Some(1..=3)) => counter(&adapters[..2], cache),
+                            // neither `prev2` or `prev1` are reachable
+                            _ => None,
+                        }
+                    }
+                    &[.., prev3, prev2, prev1, _] if prev2 < prev1 => {
+                        let ihead = adapters.len();
+
+                        match (
+                            head.checked_sub(prev3),
+                            head.checked_sub(prev2),
+                            head.checked_sub(prev1),
+                        ) {
+                            // `prev3`, `prev2`, & `prev1` are reachable
+                            (Some(3), Some(2), Some(1)) => counter(&adapters[..ihead - 1], cache)
+                                .and_then(|total| {
+                                    counter(&adapters[..ihead - 2], cache).map(|c| total + c)
+                                })
+                                .and_then(|total| {
+                                    counter(&adapters[..ihead - 3], cache).map(|c| total + c)
+                                }),
+                            // `prev3` is unreachable & `prev2` and `prev1` are reachable
+                            (Some(4..), Some(2..=3), Some(1..=2)) => {
+                                counter(&adapters[..ihead - 1], cache).and_then(|total| {
+                                    counter(&adapters[..ihead - 2], cache).map(|c| total + c)
+                                })
+                            }
+                            // `prev3` and `prev2` are unreachable & `prev1` is reachable
+                            (Some(4..), Some(4..), Some(1..=3)) => {
+                                counter(&adapters[..ihead - 1], cache)
+                            }
+                            // `prev3`, `prev2`, & `prev1` are unreachable
+                            _ => None,
+                        }
+                    }
+                    _ => None,
+                };
+
+                cache.insert(*head, arrangements);
+
+                arrangements
+            })
+        })
+    }
+
+    let mut cache = HashMap::new();
+    counter(&adapters, &mut cache)
+}
+
 fn part_one(adapters: &[u32]) {
-    let mut diff_freq = HashMap::new();
+    let mut frequencies = HashMap::new();
 
     for win in adapters.windows(2) {
-        diff_freq
+        frequencies
             .entry(win[1] - win[0])
             .and_modify(|freq| {
                 *freq += 1;
@@ -15,58 +78,18 @@ fn part_one(adapters: &[u32]) {
             .or_insert(1);
     }
 
-    let ans = diff_freq
+    let answer = frequencies
         .get(&1)
-        .zip(diff_freq.get(&3))
+        .zip(frequencies.get(&3))
         .map(|(diff_1, diff_3)| diff_1 * diff_3);
 
-    println!("Part One: {:?}", ans);
+    println!("Part One: {answer:?}");
 }
 
 fn part_two(adapters: &[u32]) {
-    let mut paths = 1;
-    // buffer for keeping track of newly discovered paths while iterating over available adapters
-    let mut heads = Vec::new();
+    let answer = count_arrangements(&adapters);
 
-    for (i, adp) in adapters.iter().take(adapters.len() - 1).enumerate() {
-        // get the adapters that are reachable from the current adapter
-        let span = adapters[(i + 1)..]
-            .iter()
-            .copied()
-            .take_while(|r| *r <= adp + 3)
-            .collect::<Vec<_>>();
-
-        match (span.len(), heads.len()) {
-            // if span is 1 and heads is non-empty:
-            // 1. count how many new paths are known and mulassign paths with that count
-            // 2. empty the heads buffer
-            // NOTE: a span of 1 will always be encountered on the penultimate entry in adapters
-            (1, 1..) => {
-                paths *= heads.drain(..).count();
-            }
-            // if span is >1, then add the heads of newly known paths to heads
-            (2.., _) => {
-                if heads.is_empty() {
-                    heads.extend(span);
-                } else {
-                    // to avoid unnecessary removals from heads:
-                    // 1. use the first adapter from span to overwrite all heads equal to adp
-                    // 2. count how many overwrites from (1.) occur
-                    // 3. append the rest of the adaptors count-from-(2.)-times to heads
-                    let mut count = 0;
-                    let (first, rest) = span.split_at(1);
-                    for head in heads.iter_mut().filter(|h| *h == adp) {
-                        *head = first[0];
-                        count += 1;
-                    }
-                    heads.extend(rest.iter().cycle().take(count * rest.len()));
-                }
-            }
-            (_, _) => {}
-        }
-    }
-
-    println!("Part Two: {}", paths);
+    println!("Part Two: {answer:?}");
 }
 
 fn main() -> io::Result<()> {
